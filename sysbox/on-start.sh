8ce0af68107fb5292df4ce88efa90c9dfb4e719c
ahmadsamehh <ahmadsameh99@gmail.com>
2025-05-23 17:10:08 +0300

    3 trial railway

--- sysbox/on-start.sh
+++ sysbox/on-start.sh
@@ -1,16 +1,16 @@
-# #!/bin/bash
-
-# # dockerd start
-# dockerd > /var/log/dockerd.log 2>&1 &
-# sleep 2
-
-# # pull solang image 
-# docker pull ghcr.io/hyperledger/solang@sha256:e6f687910df5dd9d4f5285aed105ae0e6bcae912db43e8955ed4d8344d49785d 
-
-# cargo make run
-
-
 #!/bin/bash
 
-# Just run the compiled backend binary (make sure it exists in final image)
-exec /app/target/release/backend --frontend_folder /app/packages/app/dist --port ${PORT}
+# dockerd start
+dockerd > /var/log/dockerd.log 2>&1 &
+sleep 2
+
+# pull solang image 
+# docker pull ghcr.io/hyperledger/solang@sha256:e6f687910df5dd9d4f5285aed105ae0e6bcae912db43e8955ed4d8344d49785d 
+
+cargo make run
+
+
+# #!/bin/bash
+
+# # Just run the compiled backend binary (make sure it exists in final image)
+# exec /app/target/release/backend --frontend_folder /app/packages/app/dist --port ${PORT}
